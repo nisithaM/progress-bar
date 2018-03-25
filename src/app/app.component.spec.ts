b5cdef1316f7f8521098b3034157e898794379da
nisitha.m <nisitha.m@aeturnum.com>
2018-03-25 21:36:06 +0530

    Unit Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpModule} from '@angular/http';
+import { ProgressService } from '../app/services/progress.service';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule, HttpClientModule, HttpModule ],
       declarations: [
         AppComponent
       ],
@@ -13,15 +19,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+  // it(`should have as title 'Progress Bar'`, async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('Progress Bar');
+  // }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Progress Bar Demo');
   }));
 });
--- src/app/services/progress.service.spec.ts
+++ src/app/services/progress.service.spec.ts
@@ -2,9 +2,13 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { ProgressService } from './progress.service';
 
+import {HttpClientModule} from '@angular/common/http';
+import {HttpModule} from '@angular/http';
+
 describe('ProgressService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientModule, HttpModule ],
       providers: [ProgressService]
     });
   });
